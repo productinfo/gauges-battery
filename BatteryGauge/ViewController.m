944f5854779cf7ef85be2e06781b93ea4a83e08e
Thomas Kelly <tkelly@scottlogic.co.uk>
2013-08-16 14:05:27 +0100

    Applies additional style options
    Adds the major/minor tick colour, and needle border styling, to match
    the blog.

--- BatteryGauge/ViewController.m
+++ BatteryGauge/ViewController.m
@@ -20,6 +20,7 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     gauge = [[SGaugeRadial alloc] initWithFrame:CGRectMake(0, 0, 300, 300) fromMinimum:@0 toMaximum:@100];
+    gauge.style = [SGaugeLightStyle new];
     gauge.center = self.view.center;
     gauge.autoresizingMask = UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleRightMargin | UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleBottomMargin;
     [self.view addSubview:gauge];
@@ -37,7 +38,9 @@
     gauge.style.tickBaselinePosition = 0.7;
     gauge.style.tickLabelColor = [UIColor colorWithHexString:@"3E9C62" andAlpha:1];
     gauge.style.tickLabelFont = [UIFont fontWithName:@"AmericanTypewriter-Bold" size:18];
-
+    gauge.style.majorTickColor = gauge.style.tickLabelColor;
+    gauge.style.minorTickColor = gauge.style.tickLabelColor;
+    
     //Bevel style
     gauge.style.bevelWidth = 20;
     gauge.style.bevelPrimaryColor = [UIColor colorWithHexString:@"232B5E" andAlpha:1];
@@ -48,7 +51,9 @@
     gauge.style.needleWidth = 10;
     gauge.style.needleLength = 0.75;
     gauge.style.knobColor = [UIColor colorWithHexString:@"5563BD" andAlpha:1];
-     
+    gauge.style.needleBorderColor = [UIColor blackColor];
+    gauge.style.needleBorderWidth = 1;
+
     //Observe the battery level
     [[UIDevice currentDevice] setBatteryMonitoringEnabled:YES];
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(updateBatteryStatus) name:UIDeviceBatteryLevelDidChangeNotification object:nil];
